cc339eb324f12e995395c88bac3d4c6448b5ff22
DESKTOP-0AE3V5P\Tegra <bruno.vieira@tegra.com.br>
2021-02-23 11:08:56 -0300

    JavaScript: Programando a Orientação a Objetos JavaScript: Atributos Privados

--- index.js
+++ index.js
@@ -6,20 +6,31 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
 
     sacar(valor) {
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
+        if(this._saldo >= valor) {
+            this._saldo -= valor;
         }
     }
+
+    deposicar(valor) {
+        if(valor > 0) {
+            this._saldo += valor;
+            console.log(this._saldo);
+        }
+    }    
 }
 
 const cliente1 = new Cliente();
 cliente1.nome = 'Ricardo';
 cliente1.cpf = 32487178825;
 cliente1.contaCorrente.agencia = 1001;
-cliente1.contaCorrente.saldo = 100;
+// # cliente1.contaCorrente.saldo = 100;
+
+cliente1.contaCorrente.deposicar(100);
+cliente1.contaCorrente.deposicar(200);
+cliente1.contaCorrente.deposicar(-1);
 
 cliente1.contaCorrente.sacar(20);
 
